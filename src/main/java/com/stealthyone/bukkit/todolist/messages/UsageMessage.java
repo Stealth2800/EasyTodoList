165e65119ee87eb8e06d9baa31a1c94eb71ecb33
Austin T./Stealth2800 <stealth2800@stealthyone.com>
2013-03-09 20:40:05 -0800

    Cleanup, removed ability to tag usage messages

--- src/main/java/com/stealthyone/bukkit/todolist/messages/UsageMessage.java
+++ src/main/java/com/stealthyone/bukkit/todolist/messages/UsageMessage.java
@@ -3,24 +3,15 @@ package com.stealthyone.bukkit.todolist.messages;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
-import com.stealthyone.bukkit.todolist.BasePlugin;
-import com.stealthyone.bukkit.todolist.PluginMethods;
-
 public enum UsageMessage {
 
-	TODO(ChatColor.DARK_RED + "USAGE:" + ChatColor.RED + " /todo"),
-	TODO_ADD(ChatColor.DARK_RED + "USAGE:" + ChatColor.RED + " /todo add <message>");
+	TODO("/todo"),
+	TODO_ADD("/todo add <message>");
 	
 	private String usageMessage;
-	private boolean isTagged;
 	
 	private UsageMessage(String msg) {
-		this(msg, true);
-	}
-	
-	private UsageMessage(String msg, boolean isTagged) {
 		this.usageMessage = msg;
-		this.isTagged = isTagged;
 	}
 	
 	public final String getFullMessage() {
@@ -28,10 +19,6 @@ public enum UsageMessage {
 	}
 	
 	public final void sendTo(CommandSender sender) {
-		if (this.isTagged) {
-			PluginMethods.sendTaggedMessage(sender, this);
-		} else {
-			sender.sendMessage(this.usageMessage);
-		}
+		sender.sendMessage(ChatColor.DARK_RED + "USAGE: " + ChatColor.RED + this.usageMessage);
 	}
 }
