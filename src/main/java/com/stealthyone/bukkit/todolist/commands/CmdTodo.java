1335348d9685735480a1932c4c77e9a64a8d9a4e
Austin T./Stealth2800 <stealth2800@stealthyone.com>
2013-03-09 23:09:34 -0800

    Added setpriority subcommand

--- src/main/java/com/stealthyone/bukkit/todolist/commands/CmdTodo.java
+++ src/main/java/com/stealthyone/bukkit/todolist/commands/CmdTodo.java
@@ -1,5 +1,7 @@
 package com.stealthyone.bukkit.todolist.commands;
 
+import java.util.Arrays;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,8 +10,10 @@ import org.bukkit.command.CommandSender;
 import com.stealthyone.bukkit.todolist.BasePlugin;
 import com.stealthyone.bukkit.todolist.PluginMethods;
 import com.stealthyone.bukkit.todolist.commands.subcommands.CmdTodoAdd;
+import com.stealthyone.bukkit.todolist.commands.subcommands.CmdTodoSetpriority;
 import com.stealthyone.bukkit.todolist.commands.subcommands.CmdTodoShare;
 import com.stealthyone.bukkit.todolist.commands.subcommands.ISubCmd;
+import com.stealthyone.bukkit.todolist.messages.GenericMessage;
 import com.stealthyone.bukkit.todolist.messages.UsageMessage;
 
 public final class CmdTodo implements CommandExecutor {
@@ -49,7 +53,48 @@ public final class CmdTodo implements CommandExecutor {
 				ISubCmd command = new CmdTodoShare(plugin);
 				command.run(sender, args);
 				return true;
-			} else if (args[0].equalsIgnoreCase("reload")) {
+			} else if (args[0].equalsIgnoreCase("setpriority")) {
+				/*
+				 * Sets the priority of a ticket for a player
+				 */
+				if (args.length < 2) {
+					/* Show usage */
+					UsageMessage.TODO_SETPRIORITY.sendTo(sender);
+					return true;
+				}
+				
+				ISubCmd command = new CmdTodoSetpriority(plugin);
+				command.run(sender, args);
+				return true;
+			} else if (args[0].equalsIgnoreCase("check")) {
+				/*
+				 * Shows all the tasks a player has
+				 */
+				
+				int maxPages = 1;
+				int pageNum = 1;
+				
+				if (args.length > 2) {
+					/* Show usage */
+					UsageMessage.TODO_CHECK.sendTo(sender);
+					return true;
+				} else if (args.length == 2) {
+					try {
+						Integer.valueOf(args[1]);
+					} catch (NumberFormatException e) {
+						GenericMessage.ITEM_MUST_BE_INT.sendTo(sender, Arrays.asList("{ITEM}|Page"));
+						return true;
+					}
+				}
+				
+				sender.sendMessage(ChatColor.GREEN + "----Tasks pg." + pageNum + "/" + maxPages + "----");
+				/*for (int i = 1; i <= 5; i++) {
+					String taskMessage
+					sender.sendMessage(ChatColor.GOLD + Integer.toString(i * pageNum) + ":" + ChatColor.AQUA + taskMessage);
+				}*/
+				
+				return true;
+			} else if (args[0].equalsIgnoreCase("reload")) { 
 				/* Reloads config from file */
 				PluginMethods.sendTaggedMessage(sender, ChatColor.RED + "Reloading config...");
 				plugin.reloadConfig();
