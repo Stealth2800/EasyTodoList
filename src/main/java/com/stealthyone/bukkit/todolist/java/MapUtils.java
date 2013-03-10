c8e99fa1310f0b0899bfdcd503441f44701b6961
Austin T./Stealth2800 <stealth2800@stealthyone.com>
2013-03-09 22:44:36 -0800

    Added NPE safety

--- src/main/java/com/stealthyone/bukkit/todolist/java/MapUtils.java
+++ src/main/java/com/stealthyone/bukkit/todolist/java/MapUtils.java
@@ -6,8 +6,12 @@ public final class MapUtils {
 
 	public final static int getLocationInMap(Object object, Map<String, Object> map) {
 		for (int i = 0; i < map.size(); i++) {
-			if (map.get(Integer.toString(i)).equals(object)) {
-				return i;
+			try {
+				if (map.get(Integer.toString(i)).equals(object)) {
+					return i;
+				}
+			} catch (NullPointerException e) {
+				return -1;
 			}
 		}
 		return -1;
